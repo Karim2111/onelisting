b8e71272edb23ece94f3979300dcd3b3fbd03598
liamdty <liamdty@gmail.com>
2024-11-03 16:58:04 -0500

    triangles fix

--- src/app/api/facebookSDK/loader.ts
+++ src/app/api/facebookSDK/loader.ts
@@ -16,26 +16,25 @@ export default function FacebookSDKLoader() {
 
     script.onload = () => {
       window.fbAsyncInit = function () {
-        // Use the FB type from @types/facebook-js-sdk
         FB.init({
-          appId: env.FACEBOOK_APP_ID, // keep private
+          appId: env.FACEBOOK_APP_ID,
           xfbml: true,
           version: 'v21.0',
         });
-        FB.login(function(response) {
-            if (response.authResponse) {
-             console.log('Welcome!  Fetching your information.... ');
-             FB.api('/me', function(response) {
-               console.log('Good to see you, ' + response.name + '.');
-               console.log(response);
-             });
-            } else {
-             console.log('User cancelled login or did not fully authorize.');
-            }
-        });
+
+        /*FB.login((response: fb.AuthResponse) => {
+          if (response.authResponse) {
+            console.log('Welcome! Fetching your information....');
+            FB.api('/me', (userResponse: { name: string }) => {
+              console.log('Good to see you, ' + userResponse.name + '.');
+              console.log(userResponse);
+            });
+          } else {
+            console.log('User cancelled login or did not fully authorize.');
+          }
+        }); */
       };
     };
-    
 
     document.body.appendChild(script);
   }, []);
